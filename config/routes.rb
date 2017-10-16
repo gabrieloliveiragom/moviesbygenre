d3a7e282e4567ee52fa529c5c2b63f3a52619c35
Gabriel Oliveira Martins <gabriel.oliveiragom@gmail.com>
2017-10-16 22:17:54 +0200

    Adding icon to the page and creating home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'movies/index'
   get 'movies/search' => 'movies#search'
-  get 'movies' => 'movies#search'
-  root 'movies#search'
+  get 'movies' => 'movies#index'
+  root 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
