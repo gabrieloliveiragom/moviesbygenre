0f50d01912199f834106056d5f628f0ebee96ae0
Gabriel Oliveira <gabolvrm@gmail.com>
2021-03-01 13:59:56 +0100

    Change min_votes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie
 	include HTTParty
 
 	key_value = ENV['TMDB_KEY']
-	min_votes = 1000
+	min_votes = 400
 
 	base_uri "http://api.themoviedb.org"
 	default_params api_key: key_value, language: 'en-US', sort_by: 'vote_average.desc', include_adult: false, include_video: false, 'vote_count.gte' => min_votes
